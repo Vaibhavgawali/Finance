100b873ba4997e132cf3b33734e809ffa1a86c83
saurabhWakde <128630330+saurabhWakde@users.noreply.github.com>
2024-04-29 13:20:44 +0530

    password show and hide fucntion added

--- resources/views/dashboard/password/reset-password.blade.php
+++ resources/views/dashboard/password/reset-password.blade.php
@@ -12,38 +12,85 @@
             <div class="card">
                 <div class="card-body">
 
-                    <form  class="form-sample"  id="reset_password">
+                    <form class="form-sample" id="reset_password">
                         <div id="error-container" class="alert alert-danger" style="display: none;"></div>
                         <div class="form-group row">
                             <label for="title" class="col-sm-3 col-form-label">Email ID</label>
                             <div class="col-sm-9">
-                            <input type="text" name="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Registered Email ID">
+                                <input type="text" name="email" class="form-control" id="email"
+                                    aria-describedby="emailHelp" placeholder="Registered Email ID">
                                 <div id="email_error"></div>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="description" class="col-sm-3 col-form-label">Old Password</label>
                             <div class="col-sm-9">
-                            <input type="password" name="old_password" class="form-control" id="old_password" aria-describedby="passwordHelp" placeholder="Enter Your Old Password">
-                               <div id="old_password_error"></div>
+                                <div class="input-group">
+                                    <input type="password" class="form-control p-3 toggle-password" name="old_password"
+                                        id="old_password" aria-describedby="passwordHelp"
+                                        placeholder="Enter Your Old Password">
+                                    <div class="input-group-append">
+                                        <button class="btn btn btn-gradient-primary p-3 toggle-button" type="button">
+                                            <i class="mdi mdi-eye"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div id="old_password_error"></div>
                             </div>
                         </div>
                         <div class="form-group row">
                             <label for="description" class="col-sm-3 col-form-label">New Password</label>
                             <div class="col-sm-9">
-                            <input type="password" name="new_password" class="form-control" id="new_password" aria-describedby="passwordHelp" placeholder="Enter Your New Password">
-                               <div id="new_password_error"></div>
+                                <div class="input-group">
+                                    <input type="password" name="new_password p-3" class="form-control toggle-password"
+                                        id="new_password" aria-describedby="passwordHelp"
+                                        placeholder="Enter Your New Password">
+                                    <div class="input-group-append">
+                                        <button class="btn btn btn-gradient-primary p-3 toggle-button" type="button">
+                                            <i class="mdi mdi-eye"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                                <div id="new_password_error"></div>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="password_confirmation" class="col-sm-3 col-form-label">New Confirm Password</label>
+                            <label for="description" class="col-sm-3 col-form-label">New Confirm Password</label>
                             <div class="col-sm-9">
-                                <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" aria-describedby="passwordHelp" placeholder="Enter Your New Confirm Password">
+                                <div class="input-group">
+                                    <input type="password" name="password_confirmation"
+                                        class="form-control p-3 toggle-password" id="password_confirmation"
+                                        aria-describedby="passwordHelp" placeholder="Enter Your New Confirm Password">
+                                    <div class="input-group-append">
+                                        <button class="btn btn btn-gradient-primary p-3 toggle-button" type="button">
+                                            <i class="mdi mdi-eye"></i>
+                                        </button>
+                                    </div>
+                                </div>
                                 <div id="password_confirmation_error"></div>
                             </div>
                         </div>
 
-                        <button type="submit" id="reset_password_button" class="btn btn-gradient-primary me-2">Reset Password</button>
+                        <script>
+                            $(document).ready(function() {
+                                $('.toggle-button').click(function() {
+                                    var passwordField = $(this).closest('.input-group').find('.toggle-password');
+                                    var passwordFieldType = passwordField.attr('type');
+                                    if (passwordFieldType === 'password') {
+                                        passwordField.attr('type', 'text');
+                                        $(this).find('i').removeClass('mdi mdi-eye').addClass('mdi mdi-eye-off');
+                                    } else {
+                                        passwordField.attr('type', 'password');
+                                        $(this).find('i').removeClass('mdi mdi-eye-off').addClass('mdi mdi-eye');
+                                    }
+                                });
+                            });
+                        </script>
+
+
+
+                        <button type="submit" id="reset_password_button" class="btn btn-gradient-primary me-2">Reset
+                            Password</button>
                         <a href="/dashboard" class="btn btn-light">Cancel</a>
                     </form>
                 </div>
